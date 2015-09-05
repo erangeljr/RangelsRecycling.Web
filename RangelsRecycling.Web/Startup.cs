c156210346d05f04401d35bce62ebae3699638c4
erangeljr <Hacker@eddierangel.com>
2015-09-04 22:48:24 -0700

    Initial Check In of MVC 5 Project

---
+++ RangelsRecycling.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RangelsRecycling.Web.Startup))]
+namespace RangelsRecycling.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
